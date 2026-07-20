5aa2f74faa5079f9cfdd492fb75d276115095993
Abdulraheem Habib Abiola <64592361+theAbiola@users.noreply.github.com>
2026-07-20 14:04:33 +0100

    initial commit for ci demo

---
+++ app.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest') // for sending a fake HTTP request to our ExpressJS app without starting a server on a port. That’s why app.js exports the Express App and server.js (the part of the App that calls app.listen()) is kept separate
+const { describe, it, expect } = require('@jest/globals')
+const app = require('./app')
+
+
+describe('GET /', () => { // describe() is a way to group related tests
+    it(`should return 'Works on my machine.'`, async () => { // the it() block is just one test case
+        const res = await request(app)
+        .get('/') // the it() block sends a GET request
+        .expect(200) // the it() closk expects a 200 response
+
+        expect(res.text).toBe('Works on my machine.') // we confirm that the response body text is exactly `Works on my machine.`
+    })
+})
