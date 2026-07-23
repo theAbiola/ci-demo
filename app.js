fdddd0c73adf43704b744548f90268ccfd8c1531
Abdulraheem Habib Abiola <64592361+theAbiola@users.noreply.github.com>
2026-07-23 10:52:33 +0100

    add eslint to codebase, deliberately fail linting

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
+    let msg = 'Did we get here?';
+    console.log(msg);
     res.send('Works on my machine.');
 });
 
--- server.js
+++ server.js
@@ -3,5 +3,6 @@ const app = require('./app');
 const PORT = 3000;
 
 app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server listening on port ${PORT}!`);
 });
